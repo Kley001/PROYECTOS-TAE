61a1ed9b7d22833e531bbd1bb18ef6943a938596
Daniel Toro <daatoroag@unal.edu.co>
2021-12-03 19:24:11 -0500

    Avance con bases, .shp, markdown y app inicializada

--- shiny-test/app.R
+++ shiny-test/app.R
@@ -1,49 +1,96 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinythemes)
+library(rsconnect)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("yeti"),
+                tags$head(
+                # Note the wrapping of the string in HTML()
+                tags$style(HTML("<pre>
+                .video-responsive {
+                position: relative;
+                padding-bottom: 56.25%; /* 16/9 ratio */
+                padding-top: 30px; /* IE6 workaround*/
+                height: 0;
+                overflow: hidden;
+                }
+                
+                .video-responsive iframe,
+                .video-responsive object,
+                .video-responsive embed {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                }</pre>"))),
+                navbarPage(
+                  "Accidentalidad-Medallo",
+                  tabPanel("Inicio",
+                           fluidRow(
+                             column(4,
+                                    h2("Presentado por:"),
+                                    p("Juan Esteban Carvajal Molina"),
+                                    p("Jelssin Donnovan Robledo Mena"),
+                                    p("Jesús David Santos Montes"),
+                                    p("Daniel Andrés Toro Aguirre"),
+                                    p("Kleider Stiven Vásquez Gómez"),
+                             ),
+                             column(8,
+                                    h1("Video explicativo web app"),
+                                    HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=46" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+                                    # responsive HTML('<pre><div class="video-responsive"><iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=46" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><pre>'),
+                             )
+                           ),
+                  ),
+                  tabPanel("Resumen",
+                           sidebarPanel(
+                             tags$h3("Input:"),
+                             textInput("txt1", "Given Name:", ""),
+                             textInput("txt2", "Surname:", ""),
+                             
+                           ),
+                           mainPanel(
+                             h1("Header 1"),
+                             
+                             h4("Output 1"),
+                             verbatimTextOutput("txtout"),
+                             
+                           ) 
+                           
+                  ),
+                  tabPanel("Historico", 
+                           #sidebarPanel(
+                           #tags$h3("Input:"),
+                           #dateInput("desde", "desde:"),
+                           #dateInput("hasta", "hasta:"),
+                           #dateRangeInput("fechas", label = h3("Mostrar entre")),
+                           #sliderInput("range", "Age:",min = 2014, max = 2018, value = c(2014,2018)),
+                           
+                           #), 
+                           
+                           mainPanel(
+                             h1("Video explicativo web app"),
+                             HTML('<pre><div class="video-responsive"><iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?start=46" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><pre>'),
+                             
+                             h4("Output 1"),
+                             verbatimTextOutput("txtout"),
+                             
+                           ) 
+                           ),
+                  tabPanel("Agrupamiento", "This panel is intentionally left blank"),
+                  tabPanel("Prediccion", "This panel is intentionally left blank"),
+                  tabPanel("dia a dia", "This panel is intentionally left blank"),
+                  
+                )
+) 
 
-    # Application title
-    titlePanel("proyecto uno tae"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
+ 
 server <- function(input, output) {
+  
+  output$txtout <- renderText({
+    paste( input$txt1, input$txt2, sep = " " )
+  })
+} # server
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
 shinyApp(ui = ui, server = server)
